d9f738f19a50b6cf7e4f3e249dd0d9e599152483
Tom McLaughlin <tom@serverlessops.io>
2018-08-28 17:24:50 -0500

    Initial commit

---
+++ handlers/action.py
@@ -0,0 +1,19 @@
+'''<HANDLER DESCRIPTION>'''
+
+import json
+import logging
+import os
+
+log_level = os.environ.get('LOG_LEVEL', 'INFO')
+logging.root.setLevel(logging.getLevelName(log_level))  # type: ignore
+_logger = logging.getLogger(__name__)
+
+
+def handler(event, context):
+    '''Function entry'''
+    _logger.debug('Event received: {}'.format(json.dumps(event)))
+
+    resp = {}
+    _logger.debug('Response: {}'.format(json.dumps(resp)))
+    return resp
+
