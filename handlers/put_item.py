fa9ef0192a5aed2fbf40e0e42f60da6d0f92044a
Tom McLaughlin <tom@serverlessops.io>
2018-09-03 00:13:10 -0500

    Use builtin retries from botocore.

--- handlers/put_item.py
+++ handlers/put_item.py
@@ -5,19 +5,22 @@ import logging
 import os
 
 import boto3
-from tenacity import retry, stop_after_delay, wait_random_exponential
+from botocore.config import Config
 
 log_level = os.environ.get('LOG_LEVEL', 'INFO')
 logging.root.setLevel(logging.getLevelName(log_level))  # type: ignore
 _logger = logging.getLogger(__name__)
 
+# This is actually the default per:
+# https://github.com/boto/botocore/blob/15ecfbc7ea23f81981ca65626ee166df130f64db/botocore/data/_retry.json#L119-L126
+AWS_CONFIG = Config(retries={'max_attempts': 10})
+
 # DynamoDB
 DDB_TABLE_NAME = os.environ.get('DDB_TABLE_NAME')
-dynamodb = boto3.resource('dynamodb')
+dynamodb = boto3.resource('dynamodb', config=AWS_CONFIG)
 DDT = dynamodb.Table(DDB_TABLE_NAME)
 
 
-@retry(wait=wait_random_exponential(), stop=stop_after_delay(28))
 def _put_item(item):
     '''Put record item'''
     DDT.put_item(
